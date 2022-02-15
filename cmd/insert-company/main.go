3c17a13a70b88366b23233563e3df8d913ad79fe
pedrodmvieira <pedro.vieira@bavabank.com>
2022-02-15 15:02:48 -0300

    insert-company first draft

--- cmd/insert-company/main.go
+++ cmd/insert-company/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
 func main() {
-	// deve inserir empresa
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("us-east-1"))
+	if err != nil {
+		log.Fatal("error configuring aws")
+	}
+
+	svc := dynamodb.NewFromConfig(cfg)
+
+	svc.PutItem(ctx)
 }
